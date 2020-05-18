b716fe744b231c5c1136532ef2b906041be2b197
f74064054@mail.ncku.edu.tw <f74064054@mail.ncku.edu.tw>
2020-05-18 13:45:28 +0800

    Main update

--- EatAndDrink/app/src/main/java/com/example/eatanddrink/MainActivity.java
+++ EatAndDrink/app/src/main/java/com/example/eatanddrink/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -60,12 +62,16 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         Toolbar toolbar =  (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setBackgroundColor(Color.TRANSPARENT);
         // Use toolbar as ActionBar of the app
         setSupportActionBar(toolbar);
 
         // Migrate drawerLayout and toolbar, it will show up toogle button
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
                 drawerLayout, toolbar, R.string.open, R.string.close);
+
+
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
