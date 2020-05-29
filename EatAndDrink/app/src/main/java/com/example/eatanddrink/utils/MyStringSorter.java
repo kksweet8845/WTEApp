80921e029e245d6b91641eb09770e9dbaeaf1c6a
f74064054@mail.ncku.edu.tw <f74064054@mail.ncku.edu.tw>
2020-05-29 10:26:22 +0800

    Add a String which implements parcelable interface

--- EatAndDrink/app/src/main/java/com/example/eatanddrink/utils/MyStringSorter.java
+++ EatAndDrink/app/src/main/java/com/example/eatanddrink/utils/MyStringSorter.java
@@ -1,4 +1,18 @@
 package com.example.eatanddrink.utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MyStringSorter {
+
+    private ArrayList<MyString> mList = new ArrayList<MyString>();
+
+    public MyStringSorter(ArrayList<MyString> list){
+        this.mList = list;
+    }
+
+    public ArrayList<MyString> getSortedList() {
+        Collections.sort(mList);
+        return mList;
+    }
 }
