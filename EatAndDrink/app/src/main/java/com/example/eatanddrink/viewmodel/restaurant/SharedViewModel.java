daeb237d61a88ea2872a82412fa8093994bc5573
f74064054@mail.ncku.edu.tw <f74064054@mail.ncku.edu.tw>
2020-05-29 10:30:25 +0800

    Add the shared viewmodel for potential usage

--- EatAndDrink/app/src/main/java/com/example/eatanddrink/viewmodel/restaurant/SharedViewModel.java
+++ EatAndDrink/app/src/main/java/com/example/eatanddrink/viewmodel/restaurant/SharedViewModel.java
@@ -1,6 +1,27 @@
 package com.example.eatanddrink.viewmodel.restaurant;
 
+import android.support.v4.os.IResultReceiver;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+
 public class SharedViewModel extends ViewModel {
+
+
+    private final MutableLiveData<ArrayList<DocumentSnapshot>> restaurantData = new MutableLiveData<ArrayList<DocumentSnapshot>>();
+
+    public void setRestaurantData(ArrayList<DocumentSnapshot> list){
+        restaurantData.setValue(list);
+    }
+
+    public LiveData<ArrayList<DocumentSnapshot>> getRestaurantData() {
+        return restaurantData;
+    }
+
+
 }
